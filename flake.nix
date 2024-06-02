ba8c7b5ddd1b514f5e91d120bc515ed38b839913
Shane Sveller <shanesveller@gmail.com>
2024-06-02 08:33:34 -0500

    Update nixpkgs inputs to 24.05
    Flake lock file updates:
    
    • Updated input 'darwin':
        'github:NixOS/nixpkgs/f05f3698b80b619ebf367190ddf2ccb47c851d7c' (2024-05-30)
      → 'github:NixOS/nixpkgs/75000c2cf4422c8a1776284314921ac1289c02c9' (2024-05-30)
    • Updated input 'stable':
        'github:NixOS/nixpkgs/25cf937a30bf0801447f6bf544fc7486c6309234' (2024-05-29)
      → 'github:NixOS/nixpkgs/805a384895c696f802a9bf5bf4720f37385df547' (2024-05-31)

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Empty flake to align multiple repos' nixpkgs inputs";
 
   inputs = {
-    darwin.url = "nixpkgs/nixpkgs-23.11-darwin";
-    stable.url = "nixpkgs/nixos-23.11";
+    darwin.url = "nixpkgs/nixpkgs-24.05-darwin";
+    stable.url = "nixpkgs/nixos-24.05";
     unstable.url = "nixpkgs/nixos-unstable";
   };
 
